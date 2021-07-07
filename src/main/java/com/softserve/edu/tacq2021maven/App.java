8eae2f06c8b9ef68b72bf00de254b8364492f1d8
tfesi <tsaritsyna84@gmail.com>
2021-07-07 20:01:12 +0300

    Add homework 14

---
+++ src/main/java/com/softserve/edu/tacq2021maven/App.java
@@ -0,0 +1,13 @@
+package com.softserve.edu.tacq2021maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

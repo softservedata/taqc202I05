85073b731abec365cf27125022f6946a674e62a3
Dmitriy S <sakovetsdmytryi@gmail.com>
2021-07-28 09:04:47 +0300

    added the twenty first homework

--- src/test/java/com/softserve/edu/opencart/pages/user/search/SearchSuccessPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/search/SearchSuccessPage.java
@@ -1,8 +1,20 @@
 package com.softserve.edu.opencart.pages.user.search;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchSuccessPage extends SearchCriteriaPart {
+
+    WebElement searchCriteriaField;
+    WebElement categoriesDropDownMenu;
+    WebElement searchInSubcategoriesCheckbox;
+    WebElement searchInProductDescriptionsCheckbox;
+    WebElement searchBtn;
+    WebElement productListViewBtn;
+    WebElement productGridViewBtn;
+    WebElement sortByDropdownMenu;
+    WebElement showCountDropdownMenu;
 
     public SearchSuccessPage(WebDriver driver) {
         super(driver);
@@ -11,6 +23,65 @@ public class SearchSuccessPage extends SearchCriteriaPart {
 
     private void initElements() {
         // init elements
+        searchCriteriaField = driver.findElement(By.cssSelector("#input-search"));
+        categoriesDropDownMenu = driver.findElement(By.cssSelector("div.col-sm-3 select"));
+        searchInSubcategoriesCheckbox = driver.findElement(By.cssSelector("input[name='sub_category']"));
+        searchInProductDescriptionsCheckbox = driver.findElement(By.cssSelector("input#description"));
+        searchBtn = driver.findElement(By.cssSelector("input#button-search"));
+        productListViewBtn = driver.findElement(By.cssSelector("#list-view"));
+        productGridViewBtn = driver.findElement(By.cssSelector("#grid-view"));
+        sortByDropdownMenu = driver.findElement(By.cssSelector("#input-sort"));
+        showCountDropdownMenu = driver.findElement(By.cssSelector("#input-limit"));
+    }
+
+    public void typeSearchTextOntoSearchCriteriaField(String searchText) {
+        searchCriteriaField.click();
+        searchCriteriaField.clear();
+        searchCriteriaField.sendKeys(searchText);
+    }
+
+    public void openCloseCategoriesDropDownMenu() {
+        categoriesDropDownMenu.click();
+    }
+
+    public void selectCategoryOfCategoriesDropDownMenu() {
+        //create Enum of options and make a method to choose it
+    }
+
+    public void clickSearchInSubcategoriesCheckbox() {
+        searchInSubcategoriesCheckbox.click();
+    }
+
+    public void clickSearchInProductDescriptionsCheckbox() {
+        searchInProductDescriptionsCheckbox.click();
+    }
+
+    public void clickSearchBtn() {
+        searchBtn.click();
+    }
+
+    public void clickProductListViewBtn(){
+        productListViewBtn.click();
+    }
+
+    public void clickProductGridViewBtn() {
+        productGridViewBtn.click();
+    }
+
+    public void openCloseSortByDropdownMenu() {
+        sortByDropdownMenu.click();
+    }
+
+    public void choseOptionOfSortByDropdownMenu() {
+        //create Enum of options and make a method to choose it
+    }
+
+    public void openCloseShowCountDropdownMenu() {
+        showCountDropdownMenu.click();
+    }
+
+    public void choseOptionOfShowCountDropdownMenu() {
+        //create Enum of options and make a method to choose it
     }
 
     // Page Object
